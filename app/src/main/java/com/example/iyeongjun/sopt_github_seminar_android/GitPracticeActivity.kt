0a7b9b872ce500af80fdd0743240a4d222946cd7
Woo <jhsw0375@gmail.com>
2018-06-09 15:55:47 +0900

    add-abc

---
+++ app/src/main/java/com/example/iyeongjun/sopt_github_seminar_android/GitPracticeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.iyeongjun.sopt_github_seminar_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GitPracticeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_practice)
+    }
+}
